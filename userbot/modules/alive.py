a19f4ef7061fc0cbb783b38056380de9cd1d8a4e
SIDDHARTH SRIDHAR <84910325+SRIDHAR2021SIDDHARTH@users.noreply.github.com>
2021-07-28 23:41:13 +0530

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -138,6 +138,7 @@ async def amireallyalive(alive):
         f"🐍 `Python         :` v{python_version()}\n"
         f"⚙️ `Telethon       :` v{version.__version__}\n"
         f"👤 `User           :` {DEFAULTUSER}\n"
+        f"╔══════ ≪ °❈° ≫ ══════╗\n**        🌟MAHADEV-USERBOT🌟**\n╚══════ ≪ °❈° ≫ ══════╝\n"      
         "`====================================`\n"
     )
     if ALIVE_LOGO:
