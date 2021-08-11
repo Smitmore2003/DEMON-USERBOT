64726f50fad503d3136fba0511cfbe650b2c8cf7
SIDDHARTH SRIDHAR <84910325+SRIDHAR2021SIDDHARTH@users.noreply.github.com>
2021-08-11 21:22:18 +0530

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -1,40 +1,41 @@
-# Copyright (C) 2021 The Mahadev Company LLC.
+# Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the MahaDev Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 """Userbot help command"""
 
-from userbot import CMD_HELP
+import asyncio
+from userbot import CMD_HELP, DEFAULTUSER
 from userbot.events import register
 
+modules = CMD_HELP
 
-@register(outgoing=True, pattern=r"^\.help(?: |$)(.*)")
-async def help(event):
-    """For .help command."""
+
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def help_handler(event):
+    """For .help command,"""
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("**Error!**`{args}` **not a valid module name**")
+            await event.edit(f"#WRONG\n**PLUGIN** : `{args}` ❌ **\nMohon Ketik Nama Plugin Dengan Benar.**")
+            await asyncio.sleep(200)
+            await event.delete()
     else:
-        head = "`Please specify which module do you want help for!`"
-        head2 = f"__**Loaded Modules :**__ `{len(CMD_HELP)}`"
-        head3 = "__**Function :**__ `.help` <module name>"
-        head4 = "**List for all available command below :** "
         string = ""
-        sep1 = "`••••••••••••••••••••••••••••••••••••••••••••••`"
-        sep2 = "`=========================================`"
-        for i in sorted(CMD_HELP):
+        for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`  |  "
-        await event.edit(
-            f"{head}\
-              \n{head2}\
-              \n{head3}\
-              \n{sep2}\
-              \n{head4}\
-              \n\n{string}\
-              \n{sep1}"
-        )
+            string += "`\t|  "
+        await event.edit("⚡")
+        await asyncio.sleep(2.5)
+        await event.edit(f"**[⚡Demon-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡](t.me/LynxUserbot)**\n\n"
+                         f"**◑» Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◑» Pʟᴜɢɪɴ : {len(modules)}**\n\n"
+                         "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
+                         f"╰►| {string} ◄─\n\n"
+                         f"**License : [Raphielscape Public License 1.d](https://github.com/SRIDHAR2021SIDDHARTH/DEMON-USERBOT/blob/DEMON-USERBOT/LICENSE)**\n"
+                         f"**Copyright © 2021 [Lynx-Userbot LLC Company](https://github.com/SRIDHAR2021SIDDHARTH/DEMON-USERBOT/)**")
+        await event.reply(f"\n**Example** : Type » `.help admin` For Admin Plugin Usage Information.")
+        await asyncio.sleep(1000)
+        await event.delete()
